abbe2ee1846f258633c9fdf7d6142bc8321c1273
Nil Quera <nlquera97@gmail.com>
2020-10-26 12:51:38 +0100

    Rename ReportProofs to StampProofs and add timestamp
    Add the block timestamp to event. Maybe it's not needed.

--- contracts/proofs/StampProofs.sol
+++ contracts/proofs/StampProofs.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.4.25;
 contract ReportProofs {
     uint256 public id;
     // mapping(uint256 => bytes32) private reportProofs;
-    event reportProofCreated(uint256 indexed id, bytes32 hash);
+    event reportProofCreated(uint256 indexed id, bytes32 indexed hash, uint256 timestamp);
 
     constructor() public {id = 0;}
 
     function setProof(bytes32 hash) public{
         // reportProofs[id] = hash;
-        emit reportProofCreated(id, hash);
+        emit reportProofCreated(id, hash, block.timestamp);
         ++id;
     } 
 }
