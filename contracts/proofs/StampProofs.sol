c7357e1a49958137d9448c429dd69ea10ea57564
Nil Quera <nlquera97@gmail.com>
2020-10-27 13:24:18 +0100

    Change Event name

--- contracts/proofs/StampProofs.sol
+++ contracts/proofs/StampProofs.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.4.25;
 
 contract StampProofs {
     // mapping(uint256 => bytes32) private stampProofs;
-    event stampProofCreated(uint256 indexed id, bytes32 indexed hash, uint256 timestamp);
+    event stampProof(uint256 indexed id, bytes32 indexed hash, uint256 timestamp);
 
     constructor() public {}
 
     function setProof(uint256 id, bytes32 hash) public{
         // stampProofs[id] = hash;
-        emit stampProofCreated(id, hash, block.timestamp);
+        emit stampProof(id, hash, block.timestamp);
     } 
 }
